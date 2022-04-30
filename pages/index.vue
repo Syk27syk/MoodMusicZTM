f664abe9037c94afb3e63f6889aff3aa29878e6d
shiyunkhoo <shiyun.khoo@gmail.com>
2022-04-30 22:56:37 +0800

    test commit netlify 30th april 2022

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl">Mood Music</h1>
+      <p class="mt-5">The music app that responds to your state of mind</p>
+      <p>Navigation Bar</p>
     </div>
   </div>
 </template>
