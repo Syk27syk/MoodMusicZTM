00c088db588f01a9c666c31afc175510d5dde31b
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-08 04:22:26 +0800

    audio

--- pages/playlist.vue
+++ pages/playlist.vue
@@ -3,46 +3,50 @@
         <Header />
         <MoodBot />
         <BrowseSidebar />
-        <div id="background" class="flex flex-col bg-white text-black text-center font-['nunito_sans']">
-            <div id="welcome">
-                <h5 class="uppercase"> playlists </h5>
-                <p> You are logged in. </p>
-                <button> Log out </button>
-                <button> Open Player </button>
+        <div id="background" class="flex flex-col bg-gray-100 text-black text-center font-['nunito_sans']">
+            <div id="welcome" class="my-10 border-2 border-b-gray-500 border-l-0 border-r-0 border-t-0">
+                <h5 class="uppercase text-4xl py-5 mt-20"> playlists </h5>
+                <p class="text-xl py-5"> You are logged in. </p>
+                <div class="flex gap-5 m-auto w-screen my-5 pb-10 justify-center">
+                    <button class="m-auto bg-gray-500 text-white px-5 py-3 rounded-lg"> Log out </button>
+                    <button class="m-auto bg-gray-500 text-white px-5 py-3 rounded-lg"> Open Player </button>
+                </div>
             </div>
-            <div id="playlist-main" class="m-auto h-250 w-250 border border-white border-opacity-30 rounded-md px-20 bg-white bg-opacity-20 backdrop-blur-lg">
+            <!--
+            <div id="playlist-main" class="m-auto">
                 <div id="status bar" class="flex flex-row m-auto">
                     <button id="back"><img src="" class="m-auto" alt="" /></button>
                     <p class="font-['nunito_sans'] font-bold text-2xl text-center text-black py-5 m-auto">Playing Now</p>
                     <button id="favourite"><img src="/fav.png" class="h-20 m-auto" alt="" /></button>
                 </div>
             </div>
-            <div id="playlists" class="mx-auto my-auto text-left text-black">
+            -->
+            <div id="playlists" class="mx-auto my-auto py-10 text-left">
                 <div id="playlist 1">
-                    <h6>Playlist 1 Name </h6>
+                    <h6 class="uppercase text-xl font-bold">Playlist 1 Name </h6>
                     <p> tags </p>
                     <div>
-                        <img src="" alt="" />
+                        <img src="" alt="image" />
                         <svg id="fav" />
-                        <p> Resonate </p>
+                        <p class="font-bold"> Resonate </p>
                         <p> Album: be am, 2021 </p>
                         <p> Artist: Bugge Wesseltoft</p>
-                        <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
+                        <audio id="" controls><source src="/resonate.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
                         <Controls />
                     </div>
                     <div>
-                        <img src="" alt="" />
+                        <img src="" alt="image" />
                         <svg id="fav" />
-                        <p> Distorted Light Beam </p>
+                        <p class="font-bold"> Distorted Light Beam </p>
                         <p> Album: Give Me The Future </p>
                         <p> Artist: Bastille </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
                         <Controls />
                     </div>
                     <div>
-                        <img src="" alt="" />
+                        <img src="" alt="image" />
                         <svg id="fav" />
-                        <p> All Farewells are Sudden </p>
+                        <p class="font-bold"> All Farewells are Sudden </p>
                         <p> Album: A Winged Victory for the Sullen </p>
                         <p> Artist: A Winged Victory for the Sullen </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -64,11 +68,11 @@
                     </svg>
                 </div>
                 <div>
-                    <h6>Most Frequently Played</h6>
+                    <h6 class="uppercase text-xl font-bold">Most Frequently Played</h6>
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> Brahms Piano Concerto No. 1 in D Minor</p>
+                        <p class="font-bold"> Brahms Piano Concerto No. 1 in D Minor</p>
                         <p> Artist:  </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
                         <Controls />
@@ -76,7 +80,7 @@
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> Ocean </p>
+                        <p class="font-bold"> Ocean </p>
                         <p> Album: John Butler </p>
                         <p> Artist: John Butler Trio </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -85,7 +89,7 @@
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p>  La fille aux cheveux de lin </p>
+                        <p class="font-bold">  La fille aux cheveux de lin </p>
                         <p> Album: Debussy Preludes, Book 1 </p>
                         <p> Artist: Maurizio Pollini </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -107,11 +111,11 @@
                     </svg>
                 </div>
                 <div id="recommended">
-                    <h6>Recommended For You</h6>
+                    <h6 class="uppercase text-xl font-bold">Recommended For You</h6>
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> 36 Greek Dances, AK 11, Series 1 (Excerpts Arr. C. Farmakis): No. 3, Epirotikos </p>
+                        <p class="font-bold"> 36 Greek Dances, AK 11, Series 1 (Excerpts Arr. C. Farmakis): No. 3, Epirotikos </p>
                         <p> Album: Songs &#38; Dances, 2019 </p>
                         <p> Artist: Kottos </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -120,7 +124,7 @@
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> Ghazal </p>
+                        <p class="font-bold"> Ghazal </p>
                         <p> Album: Plex, 2022 </p>
                         <p> Artist: Fazer </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -129,7 +133,7 @@
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> Say Something (Live On Later With Jools Holland </p>)
+                        <p class="font-bold"> Say Something (Live On Later With Jools Holland </p>)
                         <p> Album: Yellower, Vol. 1 - Single </p>
                         <p> Artist: Emma-Jean Thackray </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -151,11 +155,11 @@
                     </svg>
                 </div>
                 <div>
-                    <h6>New Releases</h6>
+                    <h6 class="uppercase text-xl font-bold">New Releases</h6>
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> Album: Étant donnés </p>
+                        <p class="font-bold"> Album: Étant donnés </p>
                         <p> Edouard Ferlet, Fabrice Moreau and Antoine Banville </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
                         <Controls />
@@ -163,7 +167,7 @@
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> Frostbite </p>
+                        <p class="font-bold"> Frostbite </p>
                         <p> Album: Early Hours (DJ Mix) </p>
                         <p> Artist: Shigeto </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -172,7 +176,7 @@
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> Forever </p>
+                        <p class="font-bold"> Forever </p>
                         <p> Album: Loner </p>
                         <p> Artist: Alison Wonderland </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -194,11 +198,11 @@
                     </svg>
                 </div>
                 <div>
-                    <h6>Today's Featured Music</h6>
+                    <h6 class="uppercase text-xl font-bold">Today's Featured Music</h6>
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> Still Life </p>
+                        <p class="font-bold"> Still Life </p>
                         <p> Album: Still Life - Single</p>
                         <p> Artist: Big Bang </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -207,7 +211,7 @@
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> Leave Before You Love Me </p>
+                        <p class="font-bold"> Leave Before You Love Me </p>
                         <p> Single </p>
                         <p> Artist: Marshmello and Jonas Brothers </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
@@ -216,7 +220,7 @@
                     <div>
                         <img src="" alt="" />
                         <svg id="fav" />
-                        <p> When You're Gone </p>
+                        <p class="font-bold"> When You're Gone </p>
                         <p> Single </p>
                         <p> Artist: Shawn Mendes </p>
                         <audio id="" controls><source src="/.mp3" type="audio/mp3"> Sorry, your browser doesn't support this audio.</audio>
