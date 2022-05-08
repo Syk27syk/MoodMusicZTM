0c4274dd8b490957b7eb520cb72f086dbb822efe
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-08 20:42:38 +0800

    login page

--- pages/login.vue
+++ pages/login.vue
@@ -7,10 +7,23 @@
         <div class="font-['nunito_sans'] text-xl">
           <h3 class="text-6xl font-['nunito_sans'] text-bold text-center text-amber-800  pt-16 drop-shadow-lg">Welcome!</h3>
           <h5 class="text-3xl font-alegreya text-center py-5 mb-10 drop-shadow-md">Sign up or log in to continue.</h5>
+          <div class="grid grid-cols-2 py-5 mx-auto text-left">
+            <div class="flex flex-col">
+              <div class="flex flex-row">
+                <label class="text-xl font-['nunito_sans'] font-bold" type="email" for="email">Email</label>
+                <input id="email" placeholder="email">
+              </div>
+              <div class="flex flex-row">
+                <label class="text-xl font-['nunito_sans'] font-bold" type="password" for="password">Password</label>
+                <input id="password" placeholder="password">
+              </div>
+            </div>
+            <button class="mx-auto px-5 py-3 bg-blue-500 text-white font-bold rounded-xl">Log in</button>
+          </div>
           <div>
-            <button class="w-[150px] h-[150px]"><img src="/google.png" class="drop-shadow-lg" alt="" /></button>
-            <button class="w-[150px] h-[150px]"><img src="/apple.png" class="drop-shadow-lg" alt="" /></button>
-            <button class="w-[150px] h-[150px]"><img src="/facebook.png" class="drop-shadow-lg" alt="" /></button>
+            <button class="w-[120px] h-[120px]"><img src="/google.png" class="drop-shadow-lg" alt="" /></button>
+            <button class="w-[120px] h-[120px]"><img src="/apple.png" class="drop-shadow-lg" alt="" /></button>
+            <button class="w-[120px] h-[120px]"><img src="/facebook.png" class="drop-shadow-lg" alt="" /></button>
           </div>
           <p class="my-5 text-2xl"> or </p>
           <button class="text-amber-300 text-3xl font-['nunito_sans'] text-bold text-center px-8 py-5 my-5 border-2 border-gray-100 border-opacity-50 drop-shadow-md">
@@ -40,6 +53,21 @@ export default {
           src: 'https://identity.netlify.com/v1/netlify-identity-widget.js'
         }
       ]
+    }
+  },
+  mounted() {
+    this.user = window.netlifyIdentity.currentUser();
+  },
+  methods: {
+    login() {
+      window.netlifyIdentity.open();
+      window.netlifyIdentity.on('login', (user) => {
+        this.user = user;
+      });
+    },
+    logout() {
+      window.netlifyIdentity.logout();
+      this.user = null;
     }
   },
 }
