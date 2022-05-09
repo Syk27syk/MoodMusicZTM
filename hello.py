9f81ae9839a738d85fc8fbee5bd642e42d624dc7
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-09 23:08:52 +0800

    sentiment analysis API with flask stage 1

---
+++ hello.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/')
+def home():
+    return render_template('senti.html')
+
+@app.route('/sentiment')
+def analyze():
+    args = request.args
+    s = args.get("s")
+
+    analyzer = SentimentIntensityAnalyzer()
+    scor = analyzer.polarity_scores(s)
+    return score
+
+
