9c39bfde02b6ccb22d8f67984347fb1d6d610e2a
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-10 09:08:02 +0800

    senti

---
+++ senti.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+# import natural language toolkit
+import nltk 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# download the lexicon
+nltk.download('vader_lexicon')
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/')
+def home():
+  return render_template('senti.html')
+
+@app.route('/sentiment')
+def analyze():
+  args = request.args
+  s = args.get("s")
+
+  analyzer = SentimentIntensityAnalyzer()
+  score = analyzer.polarity_scores(s)
+  return score
